ecceaaa4d920f2e3db73a7efc3e5ce39ce3aff6f
void-ness <78547746+void-ness@users.noreply.github.com>
2022-09-18 21:38:33 +0530

    fixed: font styling bug

--- scripts-js/EditFunctions.js
+++ scripts-js/EditFunctions.js
@@ -1,8 +1,10 @@
 const inputText = document.getElementsByClassName('content')[0];
-const palette=document.getElementsByClassName('palette')[0];
+const palette = document.getElementsByClassName('palette')[0];
 
 //to change the font of the input to the selected font style by the user
-inputText.style.fontFamily=`'${document.getElementById('fontType').value}'`;
+function updateText() {
+    inputText.style.fontFamily = `${document.getElementById('fontType').value}`;
+}
 
 //APPROACH: to enable the selected text formatting
 /*
@@ -27,19 +29,16 @@ inputText.style.fontFamily=`'${document.getElementById('fontType').value}'`;
 
 //APPROACH: using 'inline styling' to add functionality
 // -it solved the problem of precedence in case of classes (as it updated the property in place of overriding it)
-function editText(styleName,styleValue)
-{
-    if(eval(`inputText.style.${styleName}=='${styleValue}'`))
-    {
-        eval(`inputText.style.${styleName}=''`); 
+function editText(styleName, styleValue) {
+    if (eval(`inputText.style.${styleName}=='${styleValue}'`)) {
+        eval(`inputText.style.${styleName}=''`);
     }
-    else
-    {
+    else {
         eval(`inputText.style.${styleName}='${styleValue}'`);
     }
 }
 
 //this is to make the palette visible/hidden on request
-function showPalette(){
+function showPalette() {
     palette.classList.toggle('visible');
 }
