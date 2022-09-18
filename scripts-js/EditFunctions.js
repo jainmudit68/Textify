055036ca5e33fdde130c45dfcb54cc76480a92dc
jainmudit68 <77058106+jainmudit68@users.noreply.github.com>
2022-09-18 20:11:49 +0530

    stuck with font acceptance

--- scripts-js/EditFunctions.js
+++ scripts-js/EditFunctions.js
@@ -2,7 +2,7 @@ const inputText = document.getElementsByClassName('content')[0];
 const palette=document.getElementsByClassName('palette')[0];
 
 //to change the font of the input to the selected font style by the user
-inputText.style.fontFamily=document.getElementById('fontType').value
+inputText.style.fontFamily=`'${document.getElementById('fontType').value}'`;
 
 //APPROACH: to enable the selected text formatting
 /*
